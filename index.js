804193758a2e0f79fc76dfdf8bb9579fce745f0c
roksana35 <roksana7235@gmail.com>
2024-04-28 01:12:39 +0600

    use app.post to save data to database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const cors =require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port=process.env.PORT ||5000;
@@ -8,6 +9,52 @@ const port=process.env.PORT ||5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// arts-server
+
+// gUaF99H4HkCyNmIG
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qmgfwvr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+     
+    const artscraftCollection=client.db('artsaDB').collection('arts');
+    app.get('/addcraft',async(req,res)=>{
+      const cursor=artscraftCollection.find();
+      const result=await cursor.toArray();
+      res.send
+    })
+
+    app.post('/addcraft',async(req,res)=>{
+      const newCraft=req.body;
+      console.log(newCraft);
+      const result=await artscraftCollection.insertOne(newCraft);
+      res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/',(req,res)=>{
     res.send('arts and craft server is runing')
